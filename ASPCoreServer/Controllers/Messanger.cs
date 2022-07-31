43bfdf7b68affed1d3e401266d1585c19dddee18
lzy77_ <channelforletsplayshik@gmail.com>
2022-07-31 17:17:01 +0300

    Add global ListOfMessages. Update Get and Post methods. Rename Post into SendMessage.

--- ASPCoreServer/Controllers/Messanger.cs
+++ ASPCoreServer/Controllers/Messanger.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MyMessager;
+using Newtonsoft.Json;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ASPCoreServer.Controllers
@@ -8,18 +9,34 @@ namespace ASPCoreServer.Controllers
     [ApiController]
     public class Messanger : ControllerBase
     {
-
+        static List<Message> ListOfMessages = new List<Message>();
+        
         // GET api/<Messanger>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Response to GET " + id.ToString();
+            string output = "Not found";
+            if (id < ListOfMessages.Count && id >= 0)
+            {
+                output = JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+
+            Console.WriteLine(String.Format("Requested message №{0} : {1}", id, output));
+            return output;
         }
 
         // POST api/<Messanger>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message msg)
         {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+
+            ListOfMessages.Add(msg);
+            Console.WriteLine(String.Format("Total messages {0} \nRecieved message: {1}", ListOfMessages.Count, msg));
+            return new OkResult();
         }
     }
 }
