f52f875194cb40f69a6220d6cdc6f344e16dc956
lzy77_ <channelforletsplayshik@gmail.com>
2022-08-01 18:04:30 +0300

    Add MessangerClientAPI class in client. Update Program class of client.

---
+++ ConsoleClient/MessangerClientAPI.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMessager
+{
+    internal class MessangerClientAPI
+    {
+        public void TestNewtonsoftJson()
+        {
+            Message msg = new Message(userName: "lzy77_", messageText: "Hello!", timeStamp: DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message? deseriazebledMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deseriazebledMsg);
+
+            //{ "UserName":"lzy77_","MessageText":"Hello!","TimeStamp":"2022-07-31T11:15:57.4803287Z"}
+            //lzy77_ < вс 31.07.22 11:15:57 >: Hello!
+        }
+
+        public Message? GetMessage(int messageId)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger/" + messageId.ToString());
+            request.Method = "GET";
+            WebResponse response = request.GetResponse();
+            string status = ((HttpWebResponse)response).StatusDescription;
+            Stream output = response.GetResponseStream();
+            StreamReader reader = new StreamReader(output);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            output.Close();
+            response.Close();
+            
+            if ((status.ToLower() == "ok") && (responseFromServer != "Not found"))
+            {
+                return JsonConvert.DeserializeObject<Message>(responseFromServer);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool SendMessage(Message msg)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            string json = JsonConvert.SerializeObject(msg);
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = data.Length;
+            Stream stream = request.GetRequestStream();
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+            WebResponse response = request.GetResponse();
+            string status = ((HttpWebResponse)response).StatusDescription;
+            stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string responseFromServer = reader.ReadToEnd();
+            response.Close();
+            reader.Close();
+            stream.Close();
+            return (status.ToLower() == "ok");
+        }
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -5,16 +5,45 @@ namespace MyMessager
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessangerClientAPI API = new();
+        
+        private static void GetNewMessages()
+        {
+            Message? msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Message msg = new Message(userName: "lzy77_", messageText: "Hello!", timeStamp: DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message? deseriazebledMsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(deseriazebledMsg);
-
+            //Message msg = new Message(userName: "lzy77_", messageText: "Hello!", timeStamp: DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+            //Message? deseriazebledMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deseriazebledMsg);
             //{ "UserName":"lzy77_","MessageText":"Hello!","TimeStamp":"2022-07-31T11:15:57.4803287Z"}
             //lzy77_ < вс 31.07.22 11:15:57 >: Hello!
+            MessageID = 1;
+            Console.WriteLine("Введите ваш ник");
+            UserName = Console.ReadLine();
+            string messageText = "";
+            while (messageText != "exit")
+            {
+                GetNewMessages();
+                Console.WriteLine("Введите сообщение");
+                messageText = Console.ReadLine();
+                if (messageText.Length > 0)
+                {
+                    Message msg = new Message(userName: UserName, messageText: messageText, timeStamp: DateTime.UtcNow);
+                    API.SendMessage(msg);
+                }
+            }
         }
     }
 }
